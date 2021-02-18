1f1a87afb9fd8e2532bce3c7608a641ef9b60d38
NickName73 <60577131+NickName73@users.noreply.github.com>
2021-02-18 21:34:37 +0300

    0.12.5r

--- scripts/planet/serpulo.js
+++
@@ -1,9 +0,0 @@
-const bmaze = new SectorPreset("magamaze", Planets.serpulo, 153);
-bmaze.captureWave = 73;
-bmaze.difficulty = 4;
-bmaze.objectives = Seq.with(
-new Objectives.SectorComplete(SectorPresets.stainedMountains),
-new Objectives.Research(Vars.content.getByName(ContentType.block, "gravillaso-molecular-core")),
-new Objectives.Research(Vars.content.getByName(ContentType.block, "gravillaso-gravitol-wall")),
-new Objectives.Research(Blocks.laserDrill)
-);
---
+++ scripts/sector-changer.js
@@ -0,0 +1,4 @@
+const driedfields = new SectorPreset("dried-fields", Planets.serpulo, 12);//Для удобства желательно 
+driedfields.alwaysUnlocked = true; // название должно совпадать с названием переменной
+driedfields.captureWave = 40;
+driedfields.difficulty = 8;
