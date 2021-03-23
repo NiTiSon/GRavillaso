58bb2d01b72cbc1dfba1159dd6db572118f9bceb
NickName73 <60577131+NickName73@users.noreply.github.com>
2021-03-23 20:07:46 +0300

    0.13.1a

--- scripts/sector-changer.js
+++ scripts/sector-changer.js
@@ -1,4 +1,4 @@
-const driedfields = new SectorPreset("dried-fields", Planets.serpulo, 12);//Для удобства желательно 
+const driedfields = new SectorPreset("dried-fields", Planets.serpulo, 156);//Для удобства желательно 
 driedfields.alwaysUnlocked = true; // название должно совпадать с названием переменной
 driedfields.captureWave = 40;
 driedfields.difficulty = 8;
