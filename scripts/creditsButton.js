0c3dcb0e8b357b86d48fa4c224418cc801d9f1d4
NickName73 <60577131+NickName73@users.noreply.github.com>
2021-03-23 20:42:50 +0300

    0.13.1.2a

--- scripts/creditsButton.js
+++ scripts/creditsButton.js
@@ -1,14 +1,14 @@
 const ui = global.ui;
-ui.addMenuButton("$ui.github", "link", () => {
+ui.addMenuButton("$ui.b-github", "link", () => {
 	Core.app.openURI("https://github.com/NickName73/Gravillaso")
 });
-ui.addMenuButton("$ui.vk", "link", () => {
+ui.addMenuButton("$ui.a-vk", "link", () => {
 	Core.app.openURI("https://vk.com/gravillaso")
 });
-ui.addMenuButton("$ui.trello", "link", () => {
+ui.addMenuButton("$ui.c-trello", "link", () => {
 	Core.app.openURI("https://trello.com/b/wT73AZQq/gravillaso")
 });
-ui.addMenuButton("$ui.discord", "link", () => {
+ui.addMenuButton("$ui.d-discord", "link", () => {
 	Core.app.openURI("data:text/html,<b>Alpha</b>")
 });
 ui.onLoad(() => {
