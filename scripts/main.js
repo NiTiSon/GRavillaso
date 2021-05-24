434348909cb2b48ad3981a476cf28ab2ebc1acc7
NickName73 <egorooz73@gmail.com>
2021-05-24 18:44:43 +0300

    0.13.9

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,4 @@ var info = Vars.mods.locateMod("gravillaso");
 info.meta.displayName = "[#51158b]G[#4d2a97]r[#493fa3]a[#4554af]v[#4169bb]i[#3d7ec7]l[#3993d3]l[#35a8df]a[#31bdeb]s[#2dd2f7]o [#e5e5e5]Mod";
 info.meta.description = "Gravillaso - mod based on magnetic materials\n\n\n[red]Developers:[] \n[#ffea96]NickName73[]\nCREAsTIVE\n[maroon]Spriters:[]\nRadmir Yakupov\nCat fat\nVeniamin Katyrev\n[salmon]Additional Content from:[]\nTheSkyFather/Minerals";
 info.meta.author = "[#efc80d]N[#f0ce0b]i[#f1d409]c[#f2da07]k[#f3e005]N[#f4e603]a[#f5ec01]me73";
+info.meta.repo = "NickName73/Gravillaso"
