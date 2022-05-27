d70d65710e352831de04c5e6e50c5778ab6c748f
NickName73 <egorooz73@gmail.com>
2022-05-27 12:34:06 +0300

    Area drill test

--- core/src/nitis/gravillaso/world/blocks/production/AreaDrill.java
+++ core/src/nitis/gravillaso/world/blocks/production/AreaDrill.java
@@ -1,11 +1,15 @@
 package nitis.gravillaso.world.blocks.production;
 
-import mindustry.Vars;
+import arc.Core;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
 import mindustry.graphics.Drawf;
-
 import mindustry.graphics.Pal;
+import mindustry.type.Item;
+import mindustry.world.Tile;
 import mindustry.world.blocks.production.Drill;
-import nitis.gravillaso.content.GRPal;
+
+import static mindustry.Vars.*;
 
 /**Mine ore inside area */
 public class AreaDrill extends Drill {
@@ -13,12 +17,78 @@ public class AreaDrill extends Drill {
 
     @Override
     public void drawPlace(int x, int y, int rotation, boolean valid) {
-        super.drawPlace(x, y, rotation, valid);
+        drawPotentialLinks(x, y);
 
-        Drawf.dashCircle(x * Vars.tilesize, y * Vars.tilesize, 40, GRPal.magneturnLight);
-        Drawf.dashCircle(x * Vars.tilesize, y * Vars.tilesize, 44, Pal.accent);
+        Tile tile = world.tile(x, y);
+        if(tile == null) return;
+
+        countOre(tile);
+
+        if(returnItem != null){
+            float width = drawPlaceText(Core.bundle.formatFloat("bar.drillspeed", 60f / (drillTime + hardnessDrillMultiplier * returnItem.hardness) * returnCount, 2), x, y, valid);
+            float dx = x * tilesize + offset - width/2f - 4f, dy = y * tilesize + offset + size * tilesize / 2f + 5, s = iconSmall / 4f;
+            Draw.mixcol(Color.darkGray, 1f);
+            Draw.rect(returnItem.fullIcon, dx, dy - 1, s, s);
+            Draw.reset();
+            Draw.rect(returnItem.fullIcon, dx, dy, s, s);
+
+            if(drawMineItem){
+                Draw.color(returnItem.color);
+                Draw.rect(itemRegion, tile.worldx() + offset, tile.worldy() + offset);
+                Draw.color();
+            }
+        }else{
+            Tile to = tile.getLinkedTilesAs(this, tempTiles).find(t -> t.drop() != null && t.drop().hardness > tier);
+            Item item = to == null ? null : to.drop();
+            if(item != null){
+                drawPlaceText(Core.bundle.get("bar.drilltierreq"), x, y, valid);
+            }
+        }
+
+        Drawf.square(x * tilesize, y * tilesize, range() + 2, 90, returnItem == null ? Pal.remove : Pal.accent);
     }
+    void countOre(Tile tile){
+        returnItem = null;
+        returnCount = 0;
 
+        oreCount.clear();
+        itemArray.clear();
+
+        float halfRange = range() / 2;
+        for (float x = -halfRange; x < halfRange; x++) {
+            for (float y = -halfRange; y < halfRange; y++) {
+                Tile blockTile = world.tileWorld(x + tile.worldx(), y + tile.worldy());
+                if (blockTile.build != null) {
+                    blockTile.build.heal(1000f);
+                }
+                if (canMine(blockTile)) {
+                    oreCount.increment(getDrop(blockTile), 0, 1);
+                }
+            }
+        }
+
+        for(Item item : oreCount.keys()){
+            itemArray.add(item);
+        }
+
+        itemArray.sort((item1, item2) -> {
+            int type = Boolean.compare(!item1.lowPriority, !item2.lowPriority);
+            if(type != 0) return type;
+            int amounts = Integer.compare(oreCount.get(item1, 0), oreCount.get(item2, 0));
+            if(amounts != 0) return amounts;
+            return Integer.compare(item1.id, item2.id);
+        });
+
+        if(itemArray.size == 0){
+            return;
+        }
+
+        returnItem = itemArray.peek();
+        returnCount = oreCount.get(itemArray.peek(), 0);
+    }
+    public float range() {
+        return (size + areaSize) * tilesize;
+    }
     public AreaDrill(String name) {
         super(name);
     }
